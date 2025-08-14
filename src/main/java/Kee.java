def744e712f0a70ad50db5be69f7a8bf24eb8e2b
CoderKee <keehuangt@gmail.com>
2025-08-14 18:14:09 +0800

    Add echo, startChat methods
    Add echo and startChat methods to Kee.java
    echo returns the input string
    startChat initializes a Scanner for user input
    and checks for "bye" to exit the chat loop

--- src/main/java/Kee.java
+++ src/main/java/Kee.java
@@ -1,20 +1,42 @@
+import java.util.Scanner;
 public class Kee {
-    private static final String chatBorder = "____________________________________________________________";
+    private static final String indent = "     ";
+    private static final String chatBorder = "     ____________________________________________________________";
+
     public static void main(String[] args) {
+        startChat();
+    }
+
+    public static void greet() {
         System.out.println(chatBorder);
+        System.out.println(indent + "Hello! I'm Kee");
+        System.out.println(indent + "What can I do for you?");
+        System.out.println(chatBorder);
+    }
+
+    public static void exit() {
+        System.out.println(chatBorder);
+        System.out.println(indent + "Bye. Hope to see you again soon!");
+        System.out.println(chatBorder);
+    }
+
+    public static void echo(String msg) {
+        System.out.println(chatBorder);
+        System.out.println(indent + msg);
+        System.out.println(chatBorder);
+    }
+
+    public static void startChat() {
+        Scanner scanner = new Scanner(System.in);
         greet();
-        System.out.println(chatBorder);
-        exit();
-        System.out.println(chatBorder);
-
-    }
-
-    private static void greet() {
-        System.out.println("Hello! I'm Kee");
-        System.out.println("What can I do for you?");
-    }
-
-    private static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+            echo(input);
+        }
+        scanner.close();
     }
 }
